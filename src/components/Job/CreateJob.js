8376ba58db386c3f2f836165d944eed5d7fa858e
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2023-01-18 22:56:17 +0530

    code cleaning

--- src/components/Job/CreateJob.js
+++ src/components/Job/CreateJob.js
@@ -150,41 +150,25 @@ function CreateJob() {
               w={"2xl"}
               onChange={(e) => setJobTitle(e.target.value)}
             />
-            {/* <Textarea
-              maxW={"2xl"}
-              focusBorderColor="#790202"
-              borderWidth={2}
-              bg="white"
-              cols={10}
-              placeholder="Job Description"
-              mt="3"
-              onChange={(e) => setJobDesc(e.target.value)}
-            /> */}
-            {/* <JoditEditor
-              ref={editor}
-              value={jobDesc}
-              onChange={(newContent) => {
-                setJobDesc(newContent);
-              }}
-            /> */}
+
             <CKEditor
               editor={ClassicEditor}
-              data="<p>Hello from CKEditor 5!</p>"
+              data=""
               onReady={(editor) => {
                 // You can store the "editor" and use when it is needed.
-                console.log("Editor is ready to use!", editor);
+                // console.log("Editor is ready to use!", editor);
               }}
               onChange={(event, editor) => {
                 const data = editor.getData();
                 // console.log({ event, editor, data });
-                console.log(typeof data);
+                // console.log(typeof data);
                 setJobDesc(editor.getData());
               }}
               onBlur={(event, editor) => {
-                console.log("Blur.", editor);
+                // console.log("Blur.", editor);
               }}
               onFocus={(event, editor) => {
-                console.log("Focus.", editor);
+                // console.log("Focus.", editor);
               }}
             />
             <Flex alignItems={"center"} mt="3" gap={2}>
